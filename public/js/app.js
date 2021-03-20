15ee636465a1f80953ff26c7768eeb91300b976a
unknown <shidengebre7@gmail.com>
2021-03-20 12:05:52 -0700

    fixed callback

--- public/js/app.js
+++ public/js/app.js
@@ -6,23 +6,16 @@ const weatherForm=document.querySelector('form')
         then((response)=>{
                 response.json().then((data)=>{
                     if(data.error){
-                        document.getElementById("display-weather-info").innerHTML="Loading..."
-                        setTimeout(()=>{
-                            
-                            call()
-                        },1000)
-                        function call(){
+                       
                             document.getElementById("display-weather-info").innerHTML=data.error
                         }
-                        
-                        
-                    }
+                    
                     else{
                         document.getElementById("display-weather-info").innerHTML="Loading..."
-                        function call(){
+                        
                             displayValue="The temprature is:"+data.temperature+"<br /> the summary of the weather is :"+data.summary
                             document.getElementById("display-weather-info").innerHTML=displayValue
-                        }
+                        
                         
                     }
                 })
