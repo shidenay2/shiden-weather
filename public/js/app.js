1108ccd1bf741101684d175fb93ed7cb79ce396b
unknown <shidengebre7@gmail.com>
2021-03-20 12:21:23 -0700

    fixed unwanted addresses

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ const weatherForm=document.querySelector('form')
     weatherForm.addEventListener('submit',(e)=>{
       e.preventDefault()
         const address=document.getElementById("address-input").value
-        fetch('http://localhost:3000/weather?address='+address).
+        fetch('/weather?address='+address).
         then((response)=>{
                 response.json().then((data)=>{
                     if(data.error){
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geoCode')
 const forcast = require('./utils/forcast')
 
 const app = express()
+const port=process.env.PORT||3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialspath=path.join(__dirname,'../templates/partials')
@@ -92,6 +93,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port'+port)
 })
